538385bf9bb0c5d26d50ee01e42dd67bfa50385e
Julia Miller <ihre.hexenhafte.hoheit@gmail.com>
2019-11-01 14:31:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ function currentLine(line){
   if (line.length === 0) return "The line is currently empty."
   let result = "The line is currently: "
   for (let i=1; i<line.length+1; i++){
-    result = result+i+". "+line[i]
+    if (i===line.length) result = result+i+". "+line[i]
+    else result = result+i+". "+line[i]+", "
   }
   return result;
 }
