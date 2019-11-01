0df307dd6dc18ddb5b2d5f13723670f9368cd2c2
Julia Miller <ihre.hexenhafte.hoheit@gmail.com>
2019-11-01 14:32:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function nowServing(katzDeliLine){
 function currentLine(line){
   if (line.length === 0) return "The line is currently empty."
   let result = "The line is currently: "
-  for (let i=1; i<line.length+1; i++){
+  for (let i=1; i<line.length; i++){
     if (i===line.length) result = result+i+". "+line[i]
     else result = result+i+". "+line[i]+", "
   }
