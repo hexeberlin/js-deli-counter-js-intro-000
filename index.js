ef2c7c88c1c68cc50daba53420aafd31c5fa4389
Julia Miller <ihre.hexenhafte.hoheit@gmail.com>
2019-11-01 14:23:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-function takeANumber(currentLine, newPerson){
-  return "Welcome, "+newPerson+". You are number "+(currentLine.length+1)+" in line."
+function takeANumber(line, newPerson){
+  line.push(newPerson)
+  return "Welcome, "+newPerson+". You are number "+line.length+" in line."
 }
